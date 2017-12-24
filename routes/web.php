258c9235ad80d1b5b5d1eb988c17c1cc14e6368e
Kadek Bagus <kadekdarma90@gmail.com>
2017-12-24 08:57:56 -0500

    infokuliner project initial commit

---
+++ routes/web.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+/*
+tables:
+- restaurants
+  restaurant_id
+  restaurant_name
+  description
+  tag_line
+  email
+  status
+
+- phone_number
+  phone_number_id
+  restaurant_id
+  phone
+  whatsapp Y/N
+  created_at
+  updated_at
+
+- social_media
+  restaurant_id
+  social_media_id
+  social_media_name
+  social_media_url
+  social_media_id
+  created_at
+  updated_at
+
+- address
+  address_id
+  address_name
+  restaurant_id
+  city_id
+  created_at
+  updated_at	
+
+- city
+  city_id
+  city_name
+  created_at
+  updated_at
+
+- review
+  review_id
+  restaurant_id
+  user_id
+  review
+  created_at
+  updated_at
+
+- rating
+  rating_id
+  restaurant_id
+  rating
+  created_at
+  updated_at
+
+- media
+  media_id
+  media_name_id : retaurant_logo, restaurant_image
+  object_id     : restaurant_id
+  object_name   : restaurant
+
+
+*/
